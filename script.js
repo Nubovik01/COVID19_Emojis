206a0d6181e145fc28ac396ffe1a2060dfa9d758
NubovikGg <50026919+NubovikGg@users.noreply.github.com>
2020-04-02 17:12:42 +0300

    v1.2

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 const request = require('request')
+const colors = require('colors');
 
 /*
 
-   @ Токен нужно получить строго из приложения COVID-19 и вставить в переменную "token_covid19" ибо с другими приложениями не работает.
-   @ Далее нужно указатъ время в миллисекундах, через которое будет изменятся эмодзи в переменную "changeOnTime". Например: 5 секунд = 5000 миллисекунд.
-   @ !!! Warning: Будут ставится рандомные эмодзи из переменной "CovidEmojies", а не по порядку.
-   @ Прямо сейчас доступно только 6 эмодзи.
-   @ Если у вас нет на компьютере программы "Node.js" то, найдите в папке со скриптом файл "node-v12.16.1-x64.msi" и откройте его. Он установит "Node.js"
-   @ Не знаете как получить токен? https://www.youtube.com/watch?v=gSB2O2TcDpE
-   @ Приложение COVID-19: https://vk.com/covid19
+  @ Токен нужно получить строго из приложения COVID-19 и вставить в переменную "token_covid19" ибо с другими приложениями не работает.
+  @ Далее нужно указатъ время в миллисекундах, через которое будет изменятся эмодзи в переменную "changeOnTime". Например: 5 секунд = 5000 миллисекунд.
+  @ !!! Warning: Будут ставится рандомные эмодзи из переменной "CovidEmojies", а не по порядку.
+  @ Прямо сейчас доступно только 6 эмодзи.
+  @ Если у вас нет на компьютере программы "Node.js" то, найдите в папке со скриптом файл "node-v12.16.1-x64.msi" и откройте его. Он установит "Node.js"
+  @ Не знаете как получить токен? https://www.youtube.com/watch?v=gSB2O2TcDpE
+  @ Приложение COVID-19: https://vk.com/covid19
 
-   @ v1.0, script by Nubovik (https://vk.com/koder_nubovik)
+  @ v1.2, script by Nubovik (https://vk.com/koder_nubovik)
+*/
 
-   @ [+] Исправлено/добавлено: 
-   @ [-] Удалено/убрано:
+/*
+
+   @ EmojiID: 1 - сижу дома
+   @ EmojiID: 2 - иногда выхожу
+   @ EmojiID: 3 - игнорирую пандемию
+   @ EmojiID: 4 - хожу на работу
+   @ EmojiID: 5 - болею
+   @ EmojiID: 6 - закупаюсь бумагой
+
 */
 
 token_covid19 = `токен_от_covid19`
-changeOnTime = `через_сколько_миллисекунд_будет_изменятся_эмодзи`
-	let CovidEmojies = [
+changeOnTime = `время_в_миллисекундах`
+	let CovidEmojiesIDs = [
 	["1"],
 	["2"],
 	["3"],
@@ -27,18 +36,18 @@ changeOnTime = `через_сколько_миллисекунд_будет_из
 	["6"]
 	]
 
-setInterval(async () => {
-	switch (CovidEmojies) {
-	case 1: NameCE = "сижу дома"; break;
-	case 2: NameCE = "иногда выхожу"; break;
-	case 3: NameCE = "игнорирую пандемию"; break;
-	case 4: NameCE = "хожу на работу"; break;
-	case 5: NameCE = "болею"; break;
-	case 6: NameCE = "закупаюсь бумагой"; break;
-	}
-	
-	let rn = Math.floor(Math.random() * CovidEmojies.length)
-	console.log(`Изменён эмодзи-статус. (ID Emoji: ${CovidEmojies[rn]}) (All IDs: ${CovidEmojies})`)
-	request.get(`https://api.vk.com/method/users.setCovidStatus?v=5.103&status_id=${CovidEmojies[rn]}&access_token=${token_covid19}`)
-	
+	let timer = setInterval(() => {
+	Data = new Date();
+	Hour = Data.getHours();
+	Minutes = Data.getMinutes();
+	Seconds = Data.getSeconds();
+
+	let rn = Math.floor(Math.random() * CovidEmojiesIDs.length)
+	console.log(`Изменён эмодзи-статус. (ID Emoji: ${CovidEmojiesIDs[rn]}) (Изменено в ${Hour}:${Minutes}:${Seconds}) (All IDs: ${CovidEmojiesIDs})`)
+	request.get(`https://api.vk.com/method/users.setCovidStatus?v=5.103&status_id=${CovidEmojiesIDs[rn]}&access_token=${token_covid19}`)
+
 }, changeOnTime);
+
+let uvedomleniya = setInterval(() => {
+	console.log('Нашли баг/ошибку? Сообщите в сообщество: https://vk.com/koder_nubovik'.green)
+}, 15000);
