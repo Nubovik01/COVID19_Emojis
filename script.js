5215d62aedd7b18e713299ead9b9c7779d97502e
Nubovik <50026919+Nubovik01@users.noreply.github.com>
2020-04-13 19:28:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,7 @@ const colors = require('colors');
 
 /* 
 
-@ Токен нужно получить строго из приложения COVID-19 и вставить в переменную "token_covid19" ибо с другими приложениями не работает. 
-@ Далее нужно указатъ время в миллисекундах, через которое будет изменятся эмодзи в переменную "changeOnTime". Например: 5 секунд = 5000 миллисекунд. 
-@ !!! Warning: Будут ставится рандомные эмодзи из переменной "CovidEmojies", а не по порядку. 
-@ Прямо сейчас доступно только 6 эмодзи. 
-@ Если у вас нет на компьютере программы "Node.js" то, найдите в папке со скриптом файл "node-v12.16.1-x64.msi" и откройте его. Он установит "Node.js" 
-@ Не знаете как получить токен? https://www.youtube.com/watch?v=gSB2O2TcDpE 
-@ Приложение COVID-19: https://vk.com/covid19 
+@ Вся информация по правильной установки скрипта находится на гитхабе https://github.com/Nubovik01/COVID19_Emojis в файле README.md
 
 @ v1.5, script by Nubovik (https://vk.com/koder_nubovik) 
 */ 
@@ -30,10 +24,7 @@ changeOnTime = `время_через_которое_будет_изменятс
 let CovidEmojiesIDs = [["1"], ["2"], ["3"], ["4"], ["5"], ["6"], ["7"], ["8"], ["9"], ["10"], ["11"], ["12"], ["13"], ["14"], ["15"], ["16"], ["17"]]
 
 let timer = setInterval(() => { 
-Data = new Date(); 
-Hour = Data.getHours(); 
-Minutes = Data.getMinutes(); 
-Seconds = Data.getSeconds(); 
+Data = new Date(); Hour = Data.getHours(); Minutes = Data.getMinutes(); Seconds = Data.getSeconds(); 
 
 let rn = Math.floor(Math.random() * CovidEmojiesIDs.length) 
 console.log(`Изменён эмодзи-статус. (ID Emoji: ${CovidEmojiesIDs[rn]}) (Изменено в ${Hour}:${Minutes}:${Seconds}) (All IDs: ${CovidEmojiesIDs})`) 
