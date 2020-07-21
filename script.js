62a12156805c78de8484486130311b5d43c61f7d
Nubovik <50026919+Nubovik01@users.noreply.github.com>
2020-07-21 22:10:34 +0300

    Update script.js
    v1.7 
    +11 emoji

--- script.js
+++ script.js
@@ -5,32 +5,21 @@ const colors = require('colors');
 
 @ Вся информация по правильной установки скрипта находится на гитхабе https://github.com/Nubovik01/COVID19_Emojis в файле README.md
 
-@ v1.5, script by Nubovik (https://vk.com/koder_nubovik) 
+@ v1.7, script by Nubovik (https://vk.com/koder_nubovik) 
 */ 
 
-/* 
-
-@ EmojiID: 1 - сижу дома; @ EmojiID: 2 - иногда выхожу; @ EmojiID: 3 - игнорирую пандемию @ EmojiID: 4 - хожу на работу
-@ EmojiID: 5 - болею; @ EmojiID: 6 - закупаюсь бумагой; @ EmojiID: 7 - выгуливаю собаку; @ EmojiID: 8 - работаю из дома
-@ EmojiID: 9 - глажу кота; @ EmojiID: 10 - спасаю жизни; @ EmojiID: 11 - у холодильника; @ EmojiID: 12 - схожу с ума
-@ EmojiID: 13 - анализирую статистику; @ EmojiID: 14 - пытаюсь вернуться домой; @ EmojiID: 15 - на балконе; @ EmojiID: 16 - побеждаю печенегов;
-@ EmojiID: 17 - строю теории заговора
-
-*/ 
-
-token_covid19 = `94376e824e31eaec3a22cd101012f49e523801136df66f2cb099297d94715cbfdd24351c41fefb391d577`
-changeOnTime = `5000` 
-let CovidEmojiesIDs = [["1"], ["2"], ["3"], ["4"], ["5"], ["6"], ["7"], ["8"], ["9"], ["10"], ["11"], ["12"], ["13"], ["14"], ["15"], ["16"], ["17"]]
+token_covid19 = `токен_от_covid19`
+changeOnTime = `5000`
+let CovidEmojiesIDs = [["1"], ["2"], ["3"], ["4"], ["5"], ["6"], ["7"], ["8"], ["9"], ["10"], ["11"], ["12"], ["13"], ["14"], ["15"], ["16"], ["17"], ["18"], ["19"], ["20"], ["21"], ["22"], ["23"], ["24"], ["25"], ["26"], ["27"], ["28"]]
 
 let timer = setInterval(() => { 
-Data = new Date(); Hour = Data.getHours(); Minutes = Data.getMinutes(); Seconds = Data.getSeconds(); 
 
 let rn = Math.floor(Math.random() * CovidEmojiesIDs.length) 
-console.log(`Изменён эмодзи-статус. (ID Emoji: ${CovidEmojiesIDs[rn]}) (Изменено в ${Hour}:${Minutes}:${Seconds}) (All IDs: ${CovidEmojiesIDs})`) 
+console.log(`Изменён эмодзи-статус. (ID Emoji: ${CovidEmojiesIDs[rn]})  (Всего 28 эмодзи)`) 
 request.get(`https://api.vk.com/method/users.setCovidStatus?v=5.103&status_id=${CovidEmojiesIDs[rn]}&access_token=${token_covid19}`) 
 
 }, changeOnTime); 
 
 let uvedomleniya = setInterval(() => { 
-console.log('Нашли баг/ошибку? Сообщите в сообщество: https://vk.com/koder_nubovik'.green) 
+console.log('Нашли баг/ошибку? Сообщите в сообщество: https://vk.com/nubovik02'.green) 
 }, 15000);
